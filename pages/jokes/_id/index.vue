475aed2b06fe75d3a97e9b7e411e711d5e26b879
ntwari <ntwariegide2@gmail.com>
2021-08-10 21:56:08 +0200

    added param handler

---
+++ pages/jokes/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <AppHeader />
+        <div class="p-4">
+            <h1 class="text-lg font-semibold text-right">Jokes</h1>
+                <div>
+                    <p>Accessed id: {{ $route.params.id }}</p>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
