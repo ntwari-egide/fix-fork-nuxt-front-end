7fde511973598fb57a8f8bebda09e53728c731d9
ntwari <ntwariegide2@gmail.com>
2021-08-12 21:57:44 +0200

    adding new  format date

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -16,6 +16,13 @@
 
 <script>
     export default{
+
+        methods: {
+            formatDate(date) {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' }
+            return new Date(date).toLocaleDateString('en', options)
+            }
+        },
         async asyncData({ $content, params }) {
             const article = await $content('articles', params.slug).fetch()
             return { article }
