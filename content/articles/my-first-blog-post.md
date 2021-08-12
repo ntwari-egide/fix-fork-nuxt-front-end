d7d2716f19e2babddba5cfaa4efde784ff369eb5
ntwari <ntwariegide2@gmail.com>
2021-08-12 22:25:55 +0200

    adding searching mechanism

--- content/articles/my-first-blog-post.md
+++ content/articles/my-first-blog-post.md
@@ -4,3 +4,23 @@ description: Learning how to use @nuxt/content to create a blog
 img: first-blog-post.jpg
 alt: my first blog post
 ---
+
+```js
+export default {
+ nuxt: 'is the best'
+}
+```
+
+```java
+System.out.println("This is java testing code")
+```
+
+```css
+.nuxt-content-highlight {
+ @apply relative;
+}
+.nuxt-content-highlight .filename {
+ @apply absolute right-0 text-gray-600 font-light z-10 mr-2 mt-1 text-sm;
+}
+
+```
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,7 +4,7 @@
         <!-- <article>
             <nuxt-content :document="article" />
         </article> -->
-        <article>
+        <article class="p-4">
             <h1>{{ article.title }}</h1>
             <p>{{ article.description }}</p>
             <img :src="article.img" :alt="article.alt" />
@@ -13,12 +13,21 @@
             <nuxt-content :document="article" />
 
             <h1>Adding code block in markdown file</h1>
+            <h1 class="font-bold">Add a search field</h1>
+            <p>The Nuxt content module gives us the possibility of searching through our articles by using the <pre>search()</pre> method.</p>        
         </article>
     </div>
 </template>
 
 <script>
     export default{
+
+        data(){
+            return {
+                searchQuery: '',
+                articles: []
+            }
+        },
 
         methods: {
             formatDate(date) {
