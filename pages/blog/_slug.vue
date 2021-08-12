cc9cb56ad43aab472ecf9cb966e8847439375cf1
ntwari <ntwariegide2@gmail.com>
2021-08-12 22:42:44 +0200

    adding searching in welcome page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -5,6 +5,7 @@
             <nuxt-content :document="article" />
         </article> -->
         <article class="p-4">
+
             <h1>{{ article.title }}</h1>
             <p>{{ article.description }}</p>
             <img :src="article.img" :alt="article.alt" />
@@ -13,40 +14,13 @@
             <nuxt-content :document="article" />
 
             <h1>Adding code block in markdown file</h1>
-            <h1 class="font-bold">Add a search field</h1>
-            <p>The Nuxt content module gives us the possibility of searching through our articles by using the <pre>search()</pre> method.</p>        
-
-            <div>
-                <input
-                v-model="searchQuery"
-                type="search"
-                autocomplete="off"
-                placeholder="Search Articles"
-                class="p-4 border mt-4"
-                />
-
-                <h2 class="font-bold"> Results({{articles.length}}) </h2>
-
-                <ul v-if="articles.length">
-                    <li class="italic" v-for="article of articles" :key="article.slug">
-                        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">{{ article.title }}
-                        </NuxtLink>
-                    </li>
-                </ul>
-            </div>
+            
         </article>
     </div>
 </template>
 
 <script>
     export default{
-
-        data(){
-            return {
-                searchQuery: '',
-                articles: []
-            }
-        },
 
         methods: {
             formatDate(date) {
@@ -57,20 +31,6 @@
         async asyncData({ $content, params }) {
             const article = await $content('articles', params.slug).fetch()
             return { article }
-        },
-        watch: {
-            async searchQuery(searchQuery){
-                if (!searchQuery) {
-                    this.articles = []
-                return
-                }
-
-                this.articles = await this.$content('articles')
-                .limit(6)
-                .search(searchQuery)
-                .fetch()
-                
-            }
         }
     }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,28 @@
     <div>
       <AppHeader />
       <Nuxt />
+        <div>
+            <h1 class="font-bold">Add a search field</h1>        
+
+            <div class="pb-4">
+                <input
+                v-model="searchQuery"
+                type="search"
+                autocomplete="off"
+                placeholder="Search Articles"
+                class="p-4 border mt-4"
+                />
+
+                <h2 class="font-bold"> Results({{articles.length}}) </h2>
+
+                <ul v-if="articles.length">
+                    <li class="italic" v-for="article of articles" :key="article.slug">
+                        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">{{ article.title }}
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,6 +37,26 @@
                     name: 'description',
                     content: 'Best place of corny jokes listing'
                 }]
+            }
+        },
+        data(){
+            return {
+                searchQuery: '',
+                articles: []
+            }
+        },
+        watch: {
+            async searchQuery(searchQuery){
+                if (!searchQuery) {
+                    this.articles = []
+                return
+                }
+
+                this.articles = await this.$content('articles')
+                .limit(6)
+                .search(searchQuery)
+                .fetch()
+                
             }
         }
     }
