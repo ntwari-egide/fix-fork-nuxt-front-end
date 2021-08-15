5c67dd9d38010a4a78fd537befc3c92035886a23
ntwari <ntwariegide2@gmail.com>
2021-08-15 15:36:24 +0200

    added content in welcome page

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
     <div>
-      <AppHeader />
-      <NavBar />
-      <Nuxt />
-        <div>
-            <h1 class="font-bold">Add a search field</h1>        
-
-            <div class="pb-4">
-                <input
-                v-model="searchQuery"
-                type="search"
-                autocomplete="off"
-                placeholder="Search Articles"
-                class="p-4 border mt-4"
-                />
-
-                <h2 class="font-bold"> Results({{articles.length}}) </h2>
-
-                <ul v-if="articles.length">
-                    <li class="italic" v-for="article of articles" :key="article.slug">
-                        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">{{ article.title }}
-                        </NuxtLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
+      
     </div>
 </template>
 
