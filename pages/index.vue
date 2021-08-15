0588dcd7ef32b194c5c019fd84ccca9a2274d499
ntwari <ntwariegide2@gmail.com>
2021-08-15 19:02:42 +0200

    added styes of center container

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,32 @@
 <template>
     <div>
-        <div class="welcome-home-page bg-blue">
-            <div class="logo-section">
-                <img src="../assets/images/logo1.svg" alt="">
+        <div class="welcome-home-page bg-blue pl-0 p-6">
+            <!-- <NavBar /> -->
+            <div class="logo-section flex items-center justify-between">
+                <img class="ml-6" src="../assets/images/logo1.svg" alt="">
+
+                <button class="cursor">
+                    <svg
+                        class="h-8 w-8 fill-current text-black"
+                        fill="none" stroke-linecap="round" 
+                        stroke-linejoin="round" stroke-width="2" 
+                        viewBox="0 0 24 24" stroke="white">
+                        <path d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
+
+            <div class="flex welcome-center-content mt-24">
+                <div class="leftcontent">
+                    <p>creative digital blog</p>
+                </div>
+                <div class="centercontainer mt-6 ml-32">
+                    <h1>Time to Get Your Code <br />Clean and in Order.</h1>
+                    <h4>The longer it takes for a bug <br />to surface, the harder it is to find.</h4>
+                </div>
+                <div class="rightcontainer ml-16">
+
+                </div>
             </div>
         </div>
     </div>
@@ -56,6 +80,42 @@
     background: url('../assets/images/welcome-bg.svg');
     background-size: cover;
     background-repeat: no-repeat;
+}
+
+.welcome-home-page .logo-section img{
+    width: 86px;
+}
+
+.welcome-center-content .rightcontainer{
+height: 326px;
+    width: 518px;
+    background: rgba(255, 255, 255, 0.185);
+}
+
+.welcome-center-content{
+    font-family: 'Manrope';
+}
+.welcome-center-content .leftcontent p{
+    margin-top: 254px;
+    transform: rotate(270deg);
+    color: #ffffffdb;
+    font-size: 13px;
+    margin-left: 14px;
+}
+
+.welcome-center-content .centercontainer h1{
+    font-size: 27px;
+    font-weight: 700;
+    color: white;
+    margin-top: 24px;
+margin-left: 37px;}
+
+.welcome-center-content .centercontainer h4{
+    font-size: 19px;
+    font-weight: 300;
+    color: #ffffffab;
+    margin-top: 24px;
+    margin-left: 37px;
 }
 
 </style>
