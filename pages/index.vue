d6a81fc78a3ec019499501b801fdf70f208c54cb
ntwari <ntwariegide2@gmail.com>
2021-08-16 13:36:08 +0200

    styling new content

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
             </div>
         </div>
 
-        <div class="grid grid-cols-3" style="transform: rotate(270deg);margin-left: -204px;width: 494px;font-family: Manrope;position: static">
+        <div class="grid grid-cols-3" style="transform: rotate(270deg);margin-left: -172px;width: 494px;font-family: Manrope;position: sticky;top: 299px;">
             <hr class="mt-3 w-2/3" />
             <p class="text-center">creative digital blog</p>
             <hr class="mt-3 w-2/3 ml-16" />
@@ -101,13 +101,21 @@
             <div class="latests-news mt-4">
                 <div class="opensource p-4">
                     <div class="center" style="text-align: center;">
-                        <p style="color: #ffffffbf" class="text-sm">Open source project</p>
+                        <div class="grid grid-cols-3">
+                            <hr class="ml-6 mt-4 w-2/3" />
+                            <p style="color: #ffffffbf" class="text-sm">Open source project</p>
+                            <hr class="mt-4 w-2/3" />
+                        </div>
                         <p style="color: #ffffffbf" class="text-sm mt-2">2 Aug.2021</p>
                         <p style="color: #ffffffbf" class="text-sm">100 comments</p>
                         <p style="color: white;font-weight: 500;padding: 22px 81px;" class="text-md mt-4">Content  AdBlocker project built in Nest Js, Mongo DB, Amazon WS, Docker, CI CD </p>
                     </div>
                 </div>
-                <h1 class="text-center mt-4 font-semibold">TRENDING POSTS</h1>
+                <div class="grid grid-cols-3">
+                    <hr class="mt-9 w-2/3 border" />
+                    <h1 class="text-center mt-4 font-semibold">TRENDING POSTS</h1>
+                    <hr class="ml-6 mt-9 border" />
+                </div>
                 <div class="mt-6">
                     <p class="text-sm" style="color: rgba(0, 0, 0, 0.726)">26 July.2021</p>
                     <div class="flex">
@@ -173,29 +181,33 @@
                      
                      <hr class="mt-4"/>
                 </div>
-                <h1 class="text-center mt-6 font-semibold">FOLLOW US</h1>
-                <div class="social-medias">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="30.212" viewBox="0 0 36 30.212">
-                    <path id="Icon_feather-twitter" data-name="Icon feather-twitter" d="M34.5,4.5a16.35,16.35,0,0,1-4.71,2.3A6.72,6.72,0,0,0,18,11.295v1.5A15.99,15.99,0,0,1,4.5,6s-6,13.5,7.5,19.5a17.46,17.46,0,0,1-10.5,3c13.5,7.5,30,0,30-17.25A6.75,6.75,0,0,0,31.38,10,11.58,11.58,0,0,0,34.5,4.5Z" transform="translate(0 -2.868)" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-                    </svg>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34.875" height="34.004" viewBox="0 0 34.875 34.004">
-                    <path id="Icon_awesome-github" data-name="Icon awesome-github" d="M11.665,27.942c0,.141-.162.253-.366.253-.232.021-.394-.091-.394-.253,0-.141.162-.253.366-.253C11.482,27.668,11.665,27.78,11.665,27.942Zm-2.187-.316c-.049.141.091.3.3.345a.33.33,0,0,0,.436-.141c.042-.141-.091-.3-.3-.366A.361.361,0,0,0,9.478,27.626Zm3.108-.12c-.2.049-.345.183-.323.345.021.141.2.232.415.183s.345-.183.323-.323S12.79,27.485,12.586,27.506ZM17.212.563A16.86,16.86,0,0,0,0,17.719,17.628,17.628,0,0,0,11.918,34.537c.9.162,1.216-.394,1.216-.851,0-.436-.021-2.841-.021-4.317,0,0-4.922,1.055-5.955-2.1,0,0-.8-2.046-1.955-2.573,0,0-1.61-1.1.113-1.083A3.711,3.711,0,0,1,8.03,25.432a3.717,3.717,0,0,0,5.126,1.47,3.908,3.908,0,0,1,1.125-2.37c-3.93-.436-7.9-1.005-7.9-7.77a5.329,5.329,0,0,1,1.659-4.141,6.643,6.643,0,0,1,.183-4.774c1.47-.457,4.852,1.9,4.852,1.9a16.611,16.611,0,0,1,8.831,0s3.382-2.363,4.852-1.9a6.64,6.64,0,0,1,.183,4.774,5.466,5.466,0,0,1,1.814,4.141c0,6.785-4.141,7.327-8.072,7.77a4.158,4.158,0,0,1,1.2,3.263c0,2.37-.021,5.3-.021,5.878,0,.457.323,1.013,1.216.851a17.466,17.466,0,0,0,11.8-16.8C34.875,7.966,26.965.563,17.212.563ZM6.834,24.813c-.091.07-.07.232.049.366.113.112.274.162.366.07.091-.07.07-.232-.049-.366C7.087,24.771,6.926,24.722,6.834,24.813Zm-.759-.57c-.049.091.021.2.162.274a.209.209,0,0,0,.3-.049c.049-.091-.021-.2-.162-.274C6.237,24.152,6.124,24.173,6.075,24.244Zm2.278,2.5c-.113.091-.07.3.091.436.162.162.366.183.457.07.091-.091.049-.3-.091-.436C8.655,26.655,8.445,26.634,8.353,26.747Zm-.8-1.034c-.113.07-.113.253,0,.415s.3.232.394.162a.321.321,0,0,0,0-.436C7.847,25.692,7.664,25.622,7.552,25.713Z" transform="translate(0 -0.563)" fill="rgba(0,0,0,0.61)"/>
-                    </svg>
+                <div style="position: sticky;top: 102px;">
+                    <h1 class="text-center mt-6 font-semibold">FOLLOW US</h1>
+                    <div class="social-medias">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="30.212" viewBox="0 0 36 30.212">
+                        <path id="Icon_feather-twitter" data-name="Icon feather-twitter" d="M34.5,4.5a16.35,16.35,0,0,1-4.71,2.3A6.72,6.72,0,0,0,18,11.295v1.5A15.99,15.99,0,0,1,4.5,6s-6,13.5,7.5,19.5a17.46,17.46,0,0,1-10.5,3c13.5,7.5,30,0,30-17.25A6.75,6.75,0,0,0,31.38,10,11.58,11.58,0,0,0,34.5,4.5Z" transform="translate(0 -2.868)" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                        </svg>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="33" height="31.5" viewBox="0 0 33 31.5">
-                    <g id="Icon_feather-linkedin" data-name="Icon feather-linkedin" transform="translate(-1.5 -1.5)">
-                        <path id="Path_58" data-name="Path 58" d="M24,12a9,9,0,0,1,9,9V31.5H27V21a3,3,0,0,0-6,0V31.5H15V21a9,9,0,0,1,9-9Z" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-                        <path id="Path_59" data-name="Path 59" d="M3,13.5H9v18H3Z" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-                        <path id="Path_60" data-name="Path 60" d="M9,6A3,3,0,1,1,6,3,3,3,0,0,1,9,6Z" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-                    </g>
-                    </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="34.875" height="34.004" viewBox="0 0 34.875 34.004">
+                        <path id="Icon_awesome-github" data-name="Icon awesome-github" d="M11.665,27.942c0,.141-.162.253-.366.253-.232.021-.394-.091-.394-.253,0-.141.162-.253.366-.253C11.482,27.668,11.665,27.78,11.665,27.942Zm-2.187-.316c-.049.141.091.3.3.345a.33.33,0,0,0,.436-.141c.042-.141-.091-.3-.3-.366A.361.361,0,0,0,9.478,27.626Zm3.108-.12c-.2.049-.345.183-.323.345.021.141.2.232.415.183s.345-.183.323-.323S12.79,27.485,12.586,27.506ZM17.212.563A16.86,16.86,0,0,0,0,17.719,17.628,17.628,0,0,0,11.918,34.537c.9.162,1.216-.394,1.216-.851,0-.436-.021-2.841-.021-4.317,0,0-4.922,1.055-5.955-2.1,0,0-.8-2.046-1.955-2.573,0,0-1.61-1.1.113-1.083A3.711,3.711,0,0,1,8.03,25.432a3.717,3.717,0,0,0,5.126,1.47,3.908,3.908,0,0,1,1.125-2.37c-3.93-.436-7.9-1.005-7.9-7.77a5.329,5.329,0,0,1,1.659-4.141,6.643,6.643,0,0,1,.183-4.774c1.47-.457,4.852,1.9,4.852,1.9a16.611,16.611,0,0,1,8.831,0s3.382-2.363,4.852-1.9a6.64,6.64,0,0,1,.183,4.774,5.466,5.466,0,0,1,1.814,4.141c0,6.785-4.141,7.327-8.072,7.77a4.158,4.158,0,0,1,1.2,3.263c0,2.37-.021,5.3-.021,5.878,0,.457.323,1.013,1.216.851a17.466,17.466,0,0,0,11.8-16.8C34.875,7.966,26.965.563,17.212.563ZM6.834,24.813c-.091.07-.07.232.049.366.113.112.274.162.366.07.091-.07.07-.232-.049-.366C7.087,24.771,6.926,24.722,6.834,24.813Zm-.759-.57c-.049.091.021.2.162.274a.209.209,0,0,0,.3-.049c.049-.091-.021-.2-.162-.274C6.237,24.152,6.124,24.173,6.075,24.244Zm2.278,2.5c-.113.091-.07.3.091.436.162.162.366.183.457.07.091-.091.049-.3-.091-.436C8.655,26.655,8.445,26.634,8.353,26.747Zm-.8-1.034c-.113.07-.113.253,0,.415s.3.232.394.162a.321.321,0,0,0,0-.436C7.847,25.692,7.664,25.622,7.552,25.713Z" transform="translate(0 -0.563)" fill="rgba(0,0,0,0.61)"/>
+                        </svg>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="31.5" height="31.5" viewBox="0 0 31.5 31.5">
-                    <path id="Icon_awesome-dev" data-name="Icon awesome-dev" d="M8.446,14.645a1.365,1.365,0,0,0-.819-.306H6.4v7.346H7.628a1.365,1.365,0,0,0,.819-.306,1.073,1.073,0,0,0,.409-.918v-4.9a1.078,1.078,0,0,0-.41-.918ZM28.413,2.25H3.087A3.087,3.087,0,0,0,0,5.33V30.67a3.087,3.087,0,0,0,3.087,3.08H28.413A3.087,3.087,0,0,0,31.5,30.67V5.33a3.087,3.087,0,0,0-3.087-3.08ZM10.842,20.474a3.212,3.212,0,0,1-3.4,3.322H4.179V12.163H7.511a3.21,3.21,0,0,1,3.331,3.324Zm7.079-6.234H14.175v2.7h2.29v2.079h-2.29v2.7h3.747V23.8H13.55a1.421,1.421,0,0,1-1.457-1.384v-8.8a1.422,1.422,0,0,1,1.386-1.455h4.443Zm7.287,8.106c-.928,2.162-2.591,1.732-3.336,0L19.164,12.165h2.29l2.089,8,2.079-8h2.291l-2.7,10.181Z" transform="translate(0 -2.25)" fill="rgba(0,0,0,0.61)"/>
-                    </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="31.5" viewBox="0 0 33 31.5">
+                        <g id="Icon_feather-linkedin" data-name="Icon feather-linkedin" transform="translate(-1.5 -1.5)">
+                            <path id="Path_58" data-name="Path 58" d="M24,12a9,9,0,0,1,9,9V31.5H27V21a3,3,0,0,0-6,0V31.5H15V21a9,9,0,0,1,9-9Z" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                            <path id="Path_59" data-name="Path 59" d="M3,13.5H9v18H3Z" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                            <path id="Path_60" data-name="Path 60" d="M9,6A3,3,0,1,1,6,3,3,3,0,0,1,9,6Z" fill="none" stroke="rgba(0,0,0,0.61)" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+                        </g>
+                        </svg>
 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="31.5" height="31.5" viewBox="0 0 31.5 31.5">
+                        <path id="Icon_awesome-dev" data-name="Icon awesome-dev" d="M8.446,14.645a1.365,1.365,0,0,0-.819-.306H6.4v7.346H7.628a1.365,1.365,0,0,0,.819-.306,1.073,1.073,0,0,0,.409-.918v-4.9a1.078,1.078,0,0,0-.41-.918ZM28.413,2.25H3.087A3.087,3.087,0,0,0,0,5.33V30.67a3.087,3.087,0,0,0,3.087,3.08H28.413A3.087,3.087,0,0,0,31.5,30.67V5.33a3.087,3.087,0,0,0-3.087-3.08ZM10.842,20.474a3.212,3.212,0,0,1-3.4,3.322H4.179V12.163H7.511a3.21,3.21,0,0,1,3.331,3.324Zm7.079-6.234H14.175v2.7h2.29v2.079h-2.29v2.7h3.747V23.8H13.55a1.421,1.421,0,0,1-1.457-1.384v-8.8a1.422,1.422,0,0,1,1.386-1.455h4.443Zm7.287,8.106c-.928,2.162-2.591,1.732-3.336,0L19.164,12.165h2.29l2.089,8,2.079-8h2.291l-2.7,10.181Z" transform="translate(0 -2.25)" fill="rgba(0,0,0,0.61)"/>
+                        </svg>
+
+                    </div>
                 </div>
+                
             </div>
         </div>
     </div>
